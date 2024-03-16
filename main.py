fec471735adda7f3d3fb4f57d977ac77394f8d6b
Keertan Balaji <narayanakeertan@gmail.com>
2024-03-16 07:54:51 +0530

    move model to a subfolder and add a separate file for the API

---
+++ main.py
@@ -0,0 +1,22 @@
+import json
+from fastapi import FastAPI
+from pydantic import BaseModel
+from model.model import predict_pipeline
+from model.model import __version__ as model_version
+
+app = FastAPI()
+
+class TextIn(BaseModel):
+    text:str
+
+class PredictionOut(BaseModel):
+    response: str
+    
+@app.get("/")
+def home():
+    return {"health_check":"OK"}
+
+@app.post('/',response_model=PredictionOut)
+async def scoring_endpoint(item:TextIn):
+    response = predict_pipeline(item.text)
+    return {"response":response}
--- model/model.py
+++ model/model.py
@@ -2,27 +2,32 @@ import tensorflow as tf
 from keras_preprocessing.text import tokenizer_from_json 
 from keras.preprocessing.sequence import pad_sequences
 import json
-
 from fastapi import FastAPI
 from pydantic import BaseModel
-app = FastAPI() 
+from pathlib import Path
+import re
+__version__ = "1.0"
+
 embed_size = 50 # how big is each word vector
 max_features = 20000 # how many unique words to use (i.e num rows in embedding vector)
 maxlen = 100 
-with open('model/utils/tokenizer.json') as f:
+
+BASEDIR = Path(__file__).resolve(strict=True).parent
+
+model = tf.keras.models.load_model(f'{BASEDIR}/checkpoints/my_model.h5')
+
+with open(f'{BASEDIR}/utils/tokenizer.json') as f:
     data = json.load(f)
     tokenizer = tokenizer_from_json(data)
 
-class request(BaseModel):
-    Message: str
-@app.post('/')
-async def scoring_endpoint(item:request):
-    model = tf.keras.models.load_model('model\checkpoints\my_model.h5')
-    x = str(item.model_dump()['Message'])
-    sample_token = tokenizer.texts_to_sequences([x])
+def predict_pipeline(text):
+    text = re.sub(r'[!@#$(),\n"%^*?\:;~`0-9]', " ", text)
+    text = re.sub(r"[[]]", " ", text)
+    text = text.lower()
+    sample_token = tokenizer.texts_to_sequences([text])
     sample_X_t = pad_sequences(sample_token, maxlen=maxlen)
     y = model.predict(sample_X_t,verbose=0)
     if y[0][1] > 0.5:
-        return {"response":"toxic"}
+        return "toxic"
     else:
-        return {"response":"safe"}
+        return "safe"
